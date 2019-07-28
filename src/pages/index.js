1326909d10dbdaaf2e434d04d22eea6986ce78f6
Jon Dujaka <jon@my-clay.com>
2019-07-28 21:53:46 +0200

    import bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Layout from '../components/layout';
 
 class Index extends React.Component {
 	render() {
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
-import Bio from '../components/bio';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -23,8 +22,6 @@ class ProjectTemplate extends React.Component {
                 <p>{project.frontmatter.date}</p>
                 <div dangerouslySetInnerHTML={{ __html: project.html }} />
                 <hr />
-                <Bio />
-
                 <ul>
                     <li>
                         {previous && (
