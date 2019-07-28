64aafad90c4c9f8f761c2f3e500ac114fe8d20c4
Jon Dujaka <jon@my-clay.com>
2019-07-28 21:47:10 +0200

    untracked files

---
+++ src/templates/project.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+import Bio from '../components/bio';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+class ProjectTemplate extends React.Component {
+    render() {
+        const project = this.props.data.markdownRemark;
+        const siteTitle = this.props.data.site.siteMetadata.title;
+        const { previous, next } = this.props.pageContext;
+
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <SEO
+                    title={project.frontmatter.title}
+                    description={
+                        project.frontmatter.description || project.excerpt
+                    }
+                />
+                <h1>{project.frontmatter.title}</h1>
+                <p>{project.frontmatter.date}</p>
+                <div dangerouslySetInnerHTML={{ __html: project.html }} />
+                <hr />
+                <Bio />
+
+                <ul>
+                    <li>
+                        {previous && (
+                            <Link to={previous.fields.slug} rel="prev">
+                                ← {previous.frontmatter.title}
+                            </Link>
+                        )}
+                    </li>
+                    <li>
+                        {next && (
+                            <Link to={next.fields.slug} rel="next">
+                                {next.frontmatter.title} →
+                            </Link>
+                        )}
+                    </li>
+                </ul>
+            </Layout>
+        );
+    }
+}
+
+export default ProjectTemplate;
+
+export const pageQuery = graphql`
+    query ProjectBySlug($slug: String!) {
+        site {
+            siteMetadata {
+                title
+                author
+            }
+        }
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            id
+            excerpt(pruneLength: 160)
+            html
+            frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+                description
+            }
+        }
+    }
+`;
