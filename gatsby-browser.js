661dbb5650a83cbfe037138a205a5e5cfb08b6f6
Jon Dujaka <jon@my-clay.com>
2019-08-02 13:47:26 +0200

    test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 // custom typefaces
 exports.onClientEntry = () => {
-  console.log("We've started!")
+  var randV = Math.floor(Math.random() * (90 - 10) + 10);
+  var randH = Math.floor(Math.random() * (90 - 10) + 10);
+  var randF = Math.random() * (7 - 1) + 1;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,7 @@ import styled from "styled-components";
 class Index extends React.Component {
 	render() {
 
-		let randV = Math.floor(Math.random() * (90 - 10) + 10);
-		let randH = Math.floor(Math.random() * (90 - 10) + 10);
-		let randF = Math.random() * (7 - 1) + 1;
+		
 
 		const Ghost = styled.h1`
 			text-align: right;
