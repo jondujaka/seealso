1b462b3c15dad1d0ed1b342aef68155b65dd8aff
Jon Dujaka <jdujaka@gmail.com>
2019-09-08 12:05:51 +0200

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,65 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import SVG from 'react-inlinesvg';
 import Layout from '../components/Layout';
 
-export const IndexPageTemplate = ({ images }) => {
-	const renderImage = (img, i) => {
-		return (
-			<div key={i} className="home-image">
-				<SVG className="svg-overlay" src={img.svg.publicURL} />
-				<Img fluid={img.mainImage.childImageSharp.fluid} />
-			</div>
-		);
-	};
+	const IndexPage = ({ data }) => {
+		const [position, setPosition] = useState({
+		opacity: 0
+	});
+
+	useEffect(() => {
+		setPosition({
+			horizontal: Math.floor(Math.random() * (70 - 10) + 10),
+			vertical: Math.floor(Math.random() * (40 - 10) + 10),
+			size: Math.random() * (7 - 1) + 1,
+			opacity: 1
+		});
+	}, []);
+
+	const Ghost = styled.h1`
+		text-align: right;
+		display: block;
+		margin-right: ${position.horizontal}vw;
+		margin-top: ${position.vertical}vh;
+		transition: 0.2s opacity 0.4s ease-in-out;
+		font-size: ${position.size}rem;
+		opacity: ${position.opacity};
+	`;
 
 	return (
-		<div className="images-wrapper">
-			{images && images.map((img, index) => renderImage(img, index))}
-		</div>
-	);
-};
-
-const IndexPage = ({ data }) => {
-	const imagesData = useStaticQuery(graphql`
-		query {
-			site {
-				siteMetadata {
-					title
-				}
-			}
-
-			# Get the home-page
-			markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-				frontmatter {
-					title
-					templateKey
-					date
-					image {
-						mainImage {
-							childImageSharp {
-								fluid(maxWidth: 2540) {
-									...GatsbyImageSharpFluid_tracedSVG
-								}
-							}
-						}
-						svg {
-							publicURL
-						}
-					}
-				}
-			}
-		}
-	`);
-
-	const imagesArray = imagesData.markdownRemark.frontmatter.image;
-
-	return (
-		<Layout showNav={true}>
-			<IndexPageTemplate images={imagesArray} />
+		<Layout showNav={false} title={`👻 - See Also`}>
+			<Ghost>
+				<span role="img">👻</span>
+			</Ghost>
 		</Layout>
 	);
+
 };
 
 export default IndexPage;
