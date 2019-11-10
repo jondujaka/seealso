2b301fb691ad9311805e0691da46480e7340eded
Jon Dujaka <jon@my-clay.com>
2019-11-10 16:42:14 +0100

    test

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -7,8 +7,17 @@ import ScrollSnap from 'scroll-snap';
 
 
 
+
+
 const callback = () => {
   console.log('Snap!')
+}
+
+const exportFunc = null;
+if(process.env.CONTEXT === 'production'){
+	exportFunc = IndexPage;
+} else {
+	exportFunc = IndexPageTeaser;
 }
 
 const initSnapScroll = () => {
@@ -18,6 +27,40 @@ const initSnapScroll = () => {
 	}
 	const snapObject = new ScrollSnap(element, snapConfig)
 	snapObject.bind(callback)
+}
+
+const IndexPageTeaser = ({data}) => {
+	const IndexPage = ({ data }) => {
+		const [position, setPosition] = useState({
+		opacity: 0
+	});
+
+	useEffect(() => {
+		setPosition({
+			horizontal: Math.floor(Math.random() * (70 - 10) + 10),
+			vertical: Math.floor(Math.random() * (40 - 10) + 10),
+			size: Math.random() * (7 - 1) + 1,
+			opacity: 1
+		});
+	}, []);
+
+	const Ghost = styled.h1`
+		text-align: right;
+		display: block;
+		margin-right: ${position.horizontal}vw;
+		margin-top: ${position.vertical}vh;
+		transition: 0.2s opacity 0.4s ease-in-out;
+		font-size: ${position.size}rem;
+		opacity: ${position.opacity};
+	`;
+
+	return (
+		<Layout showNav={false} title={`👻 - See Also`}>
+			<Ghost>
+				<span role="img">👻</span>
+			</Ghost>
+		</Layout>
+	);
 }
 
 export const IndexPageTemplate = ({ images }) => {
