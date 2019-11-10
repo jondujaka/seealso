609b833ffeb1c70e5878e18f01e25c2213240965
Jon Dujaka <jon@my-clay.com>
2019-11-10 22:08:10 +0100

    test

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,8 +1,10 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import SVG from 'react-inlinesvg';
 import Layout from '../components/Layout';
+import IndexPageTeaser from '../components/IndexPageTeaser';
+import ImageMap from '../components/ImageMap';
 import ScrollSnap from 'scroll-snap';
 
 
@@ -11,13 +13,6 @@ import ScrollSnap from 'scroll-snap';
 
 const callback = () => {
   console.log('Snap!')
-}
-
-const exportFunc = null;
-if(process.env.CONTEXT === 'production'){
-	exportFunc = IndexPage;
-} else {
-	exportFunc = IndexPageTeaser;
 }
 
 const initSnapScroll = () => {
@@ -29,61 +24,22 @@ const initSnapScroll = () => {
 	snapObject.bind(callback)
 }
 
-const IndexPageTeaser = ({data}) => {
-
-	const [position, setPosition] = useState({
-		opacity: 0
-	});
-
-	useEffect(() => {
-		setPosition({
-			horizontal: Math.floor(Math.random() * (70 - 10) + 10),
-			vertical: Math.floor(Math.random() * (40 - 10) + 10),
-			size: Math.random() * (7 - 1) + 1,
-			opacity: 1
-		});
-	}, []);
-
-	const Ghost = styled.h1`
-		text-align: right;
-		display: block;
-		margin-right: ${position.horizontal}vw;
-		margin-top: ${position.vertical}vh;
-		transition: 0.2s opacity 0.4s ease-in-out;
-		font-size: ${position.size}rem;
-		opacity: ${position.opacity};
-	`;
-
-	return (
-		<Layout showNav={false} title={`👻 - See Also`}>
-			<Ghost>
-				<span role="img">👻</span>
-			</Ghost>
-		</Layout>
-	);
-}
-
 export const IndexPageTemplate = ({ images }) => {
 
 	useEffect(() => {
 	    initSnapScroll();
 	}, []);
 
-	const renderImage = (img, i) => {
-		return (
-			<div key={i} className="home-image">
-				<SVG className="svg-overlay" src={img.svg.publicURL} />
-				<Img fluid={img.mainImage.childImageSharp.fluid} />
-			</div>
-		);
-	};
 
-	console.log(images);
-	return (
-		<div className="h-100 images-wrapper" id="scroll-container">
-			{images && images.map((img, index) => renderImage(img, index))}
-		</div>
-	);
+	if(process.env.CONTEXT === 'production'){
+		return <IndexPageTeaser/>
+	} else {
+		return(
+			<div className="h-100 images-wrapper" id="scroll-container">
+				{images && images.map((img, index) => <ImageMap key={index} images={img} />)}
+			</div>
+		)
+	}
 };
 
 const IndexPage = ({ data }) => {
@@ -127,4 +83,4 @@ const IndexPage = ({ data }) => {
 	);
 };
 
-export default exportFunc;
+export default IndexPage;
